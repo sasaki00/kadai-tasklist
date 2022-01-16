9dfea71935060b2754fc591a722cbbca897f3399
ryuta.sasaki <schny613@yahoo.co.jp>
2022-01-17 08:45:32 +0900

    EditServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -13,26 +13,30 @@ import javax.servlet.http.HttpServletResponse;
 import models.Task;
 import utils.DBUtil;
 
-@WebServlet("/show")
-public class ShowServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public ShowServlet() {
+    public EditServlet() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        // 該当のIDのタスク1件のみをデータベースから取得
+        // 該当のIDのメッセージ1件のみをデータベースから取得
         Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        // タスクデータをリクエストスコープにセットしてshow.jspを呼び出す
+        // タスク情報とセッションIDをリクエストスコープに登録
         request.setAttribute("task", m);
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
+        // タスクIDをセッションスコープに登録
+        request.getSession().setAttribute("task_id", m.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
 
