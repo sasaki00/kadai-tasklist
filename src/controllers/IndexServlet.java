4c3f986699b5e868d8cfc5957c811b920c669c33
ryuta.sasaki <schny613@yahoo.co.jp>
2022-01-16 11:47:19 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,8 +26,13 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
-        em.close();    }
+        em.close();
+
+        request.setAttribute("taskss", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
